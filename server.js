f6378078139939dbeacc6f00cabdf2afc57d9ec0
SCcagg5 <sccagg5@gmail.com>
2023-03-23 00:52:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,13 @@ const sharp = require('sharp');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.json({ limit: '50mb' })); // pour permettre de récupérer des images en base64
-
+app.use(bodyParser.json({ limit: '50mb' }));
 app.post('/double-resolution', async (req, res) => {
   try {
-    const inputImage = Buffer.from(req.body.image, 'base64'); // récupère l'image en base64
-    const image = await sharp(inputImage).resize({ width: 200 }).toBuffer(); // double la résolution de l'image
-    const base64Image = image.toString('base64'); // convertit l'image en base64
-    res.json({ image: base64Image }); // renvoie l'image en base64
+    const inputImage = Buffer.from(req.body.image, 'base64'); 
+    const image = await sharp(inputImage).resize({ width: 200 }).toBuffer(); 
+    const base64Image = image.toString('base64'); 
+    res.json({ image: base64Image }); 
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal server error' });
