5e8828a041a980ba1a0e4d7e7c7bd2834ca724bf
SCcagg5 <sccagg5@gmail.com>
2023-03-23 12:45:52 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,19 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.post('/double-resolution', async (req, res) => {
   try {
     const inputImage = Buffer.from(req.body.image, 'base64'); 
-    const image = await sharp(inputImage).resize({ width: 200 }).toBuffer(); 
-    const base64Image = image.toString('base64'); 
-    res.json({ image: base64Image }); 
+    sharp(inputImage)
+    .resize({ width: 200, withoutEnlargement: true })
+    .toBuffer((err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error processing image');
+      } else {
+        res.setHeader('Content-Type', 'image/jpeg');
+        res.setHeader('Content-Length', data.length);
+        res.end(data);
+      }
+    });
+});
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal server error' });
